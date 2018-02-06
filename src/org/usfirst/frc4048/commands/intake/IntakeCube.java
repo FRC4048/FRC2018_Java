5c1d5426b77b2597405ac67b3d9ec41d4a536d9e
Steve Nikopoulos <steve@nikopoulos.net>
2018-02-06 10:33:57 -0500

    fix and clarifications

--- src/org/usfirst/frc4048/commands/intake/IntakeCube.java
+++ src/org/usfirst/frc4048/commands/intake/IntakeCube.java
@@ -12,54 +12,64 @@ import edu.wpi.first.wpilibj.command.Command;
 public class IntakeCube extends Command {
 	private MotorUtils leftMotor;
 	private MotorUtils rightMotor;
-	private final TriggerEvent event;
-	private AdjustSide adjustSide = AdjustSide.LEFT;
+	private final IntakeMode mode;
+	private PullSide pullSide = PullSide.LEFT;
 
-	private enum AdjustSide {
-		LEFT, RIGHT, 
+	private enum PullSide {
+		LEFT, RIGHT,
 	};
-	
-	public enum TriggerEvent {
-		ADJUST,
-		INTAKE
+
+	public enum IntakeMode {
+
+		/**
+		 * Alternate pulling from one side or the other.
+		 */
+		TOGGLE_PULL_LEFT_OR_RIGHT,
+
+		/**
+		 * Both intake motors run at the same speed.
+		 */
+		STRAIGHT_PULL
 	}
 
-    public IntakeCube() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.intake);
-    	this.event = TriggerEvent.INTAKE;
-    }
+	public IntakeCube() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.intake);
+		this.mode = IntakeMode.STRAIGHT_PULL;
+	}
 
-    public IntakeCube(TriggerEvent event) {
-    	requires(Robot.intake);
-		this.event = event;
+	public IntakeCube(IntakeMode event) {
+		requires(Robot.intake);
+		this.mode = event;
 	}
 
 	// Called just before this Command runs the first time
-    protected void initialize() {
-    	setTimeout(2.0);
-    	leftMotor = new MotorUtils(RobotMap.PDP_LEFT_INTAKE_MOTOR, RobotMap.CURRENT_THRESHOLD_INTAKE_MOTOR, RobotMap.TIMEOUT_INTAKE_MOTOR);
-    	rightMotor = new MotorUtils(RobotMap.PDP_RIGHT_INTAKE_MOTOR, RobotMap.CURRENT_THRESHOLD_INTAKE_MOTOR, RobotMap.TIMEOUT_INTAKE_MOTOR);
-    }
+	protected void initialize() {
+		setTimeout(2.0);
+		leftMotor = new MotorUtils(RobotMap.PDP_LEFT_INTAKE_MOTOR, RobotMap.CURRENT_THRESHOLD_INTAKE_MOTOR,
+				RobotMap.TIMEOUT_INTAKE_MOTOR);
+		rightMotor = new MotorUtils(RobotMap.PDP_RIGHT_INTAKE_MOTOR, RobotMap.CURRENT_THRESHOLD_INTAKE_MOTOR,
+				RobotMap.TIMEOUT_INTAKE_MOTOR);
+	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		if (!Robot.intake.hasCube() && Robot.intake.isLowered() && !isTimedOut()) {
-			switch (event) {
-			case INTAKE:
+			switch (mode) {
+			case STRAIGHT_PULL:
 				Robot.intake.intakeCube();
 				break;
 
-			case ADJUST:
-				switch (adjustSide) {
+			case TOGGLE_PULL_LEFT_OR_RIGHT:
+				switch (pullSide) {
 				case LEFT:
 					Robot.intake.adjustCubeLeftSide();
-					adjustSide = AdjustSide.RIGHT;
+					pullSide = PullSide.RIGHT;
 					break;
 				case RIGHT:
 					Robot.intake.adjustCubeRightSide();
-					adjustSide = AdjustSide.LEFT;
+					pullSide = PullSide.LEFT;
 					break;
 				}
 				break;
@@ -68,19 +78,20 @@ public class IntakeCube extends Command {
 		}
 	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return Robot.intake.hasCube() || !Robot.intake.isLowered() || isTimedOut() || leftMotor.isStalled() || rightMotor.isStalled();
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Robot.intake.hasCube() || !Robot.intake.isLowered() || isTimedOut() || leftMotor.isStalled()
+				|| rightMotor.isStalled();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.intake.stopIntake();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.intake.stopIntake();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	Robot.intake.stopIntake();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		Robot.intake.stopIntake();
+	}
 }
