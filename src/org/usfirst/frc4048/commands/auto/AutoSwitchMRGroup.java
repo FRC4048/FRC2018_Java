32e8d326ce4bd5f395249a3f95f95c906d807ad7
Ben Krugman <krugmanb21@student.westboroughk12.org>
2018-02-11 16:24:54 -0500

    Middle Right command

--- src/org/usfirst/frc4048/commands/auto/AutoSwitchMRGroup.java
+++ src/org/usfirst/frc4048/commands/auto/AutoSwitchMRGroup.java
@@ -1,6 +1,12 @@
 package org.usfirst.frc4048.commands.auto;
 
+import org.usfirst.frc4048.commands.DriveDistance;
+import org.usfirst.frc4048.commands.arm.MoveArm;
+import org.usfirst.frc4048.commands.arm.OpenClaw;
+import org.usfirst.frc4048.subsystems.Arm.ArmPositions;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitForChildren;
 
 /**
  *
@@ -24,5 +30,12 @@ public class AutoSwitchMRGroup extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	
+    	//DRIVE TO SWITCH AND RAISE ARM
+    	addParallel(new DriveDistance(AutoAction.AUTO_RUN_DISTANCE, AutoAction.LOCAL_SWITCH_SPEED,0,0));
+    	addSequential(new MoveArm(ArmPositions.Switch)); //TODO add this back
+    	//WaitForChildren() waits for the parallel commands to finish
+    	addSequential(new WaitForChildren());
+    	addSequential(new OpenClaw());
     }
 }
