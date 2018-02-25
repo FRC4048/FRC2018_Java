f3d826b9547ed5e375cf2c38f6b70ff40996ee10
Matthew Spofford <matthewspofford@verizon.net>
2018-02-25 17:30:38 -0500

    Changed stall current stuff for wrist

--- src/org/usfirst/frc4048/RobotMap.java
+++ src/org/usfirst/frc4048/RobotMap.java
@@ -101,7 +101,7 @@ public class RobotMap {
     public static final double CURRENT_THRESHOLD_INTAKE_MOTOR = 10.0;
     public static final double CURRENT_THRESHOLD_INTAKE_DEPLOY_MOTOR = 25.0;
     public static final double CURRENT_THRESHOLD_ARM_CUBE_PICKUP = 10.0;
-    public static final double CURRENT_THRESHOLD_WRIST_MOTOR = 6.5;
+    public static final double CURRENT_THRESHOLD_WRIST_MOTOR = 6.0;
     public static final double CURRENT_THRESHOLD_GRIP_MOTOR = 5.5;
     /**
      * TODO Determine correct timeout for intake motors.
