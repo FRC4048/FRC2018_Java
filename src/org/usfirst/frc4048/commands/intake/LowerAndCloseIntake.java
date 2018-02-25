3d10c52c7eae59e4b0d9cab1b90fe7747a82b86a
Matthew Spofford <matthewspofford@verizon.net>
2018-02-25 17:51:24 -0500

    Fixed lower and raise intake

--- src/org/usfirst/frc4048/commands/intake/LowerAndCloseIntake.java
+++ src/org/usfirst/frc4048/commands/intake/LowerAndCloseIntake.java
@@ -36,6 +36,6 @@ public class LowerAndCloseIntake extends CommandGroup {
     	
     	addParallel(new GripIntake(callback, GripPosition.Close));
     	addSequential(new LowerIntake(callback));
-//    	addSequential(new WaitForChildren());
+    	addSequential(new WaitForChildren());
     }
 }
