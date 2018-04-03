105984386cdce64d22e6057560edbf0a8d0aeb1c
Matthew Spofford <matthewspofford@verizon.net>
2018-04-02 20:51:20 -0400

    Adjusted min and max pot values

--- src/org/usfirst/frc4048/subsystems/Arm.java
+++ src/org/usfirst/frc4048/subsystems/Arm.java
@@ -124,11 +124,11 @@ public class Arm extends Subsystem {
 	 * All of these values are used for the extension math
 	 */
 	private final double HOME_FROM_TOWER = 39.0;
-	private final double ARM_POT_MIN = 606;
-	private final double ARM_POT_MAX = 14;
+	private final double ARM_POT_MIN = 90;
+	private final double ARM_POT_MAX = 814;
 	private final double ARM_ANGLE_MIN = 0.0;
-	private final double ARM_ANGLE_MAX = 114.0;
-	private final double ARM_POT_INVERT = -1.0;
+	private final double ARM_ANGLE_MAX = 141.0;
+	private final double ARM_POT_INVERT = 1.0;
 	
 	private final double ELBW_POT_MIN = 2771.0;
 	private final double ELBW_POT_MAX = 0.0;
