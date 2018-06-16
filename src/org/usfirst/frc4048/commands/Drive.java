906038e47e05490053dbba341223a94ab42c3382
Matthew Spofford <matthewspofford@verizon.net>
2018-06-16 13:06:40 -0400

    Fixed Drivetrain becuase it was inverted

--- src/org/usfirst/frc4048/commands/Drive.java
+++ src/org/usfirst/frc4048/commands/Drive.java
@@ -14,6 +14,7 @@ public class Drive extends Command {
     public Drive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
@@ -23,7 +24,7 @@ public class Drive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	fwd = Robot.oi.getLeftJoystick().getY();
+    	fwd = -Robot.oi.getLeftJoystick().getY();
     	str = Robot.oi.getLeftJoystick().getX();
     	rcw = Robot.oi.getRightJoystick().getX();
     	
@@ -40,7 +41,7 @@ public class Drive extends Command {
     	}
     	
     	if (rcw < 0) {
-    		rcw *= rcw * 1;
+    		rcw *= rcw * -1;
     	} else {
     		rcw *= rcw;
     	}
@@ -50,18 +51,17 @@ public class Drive extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	Robot.drivetrain.move(0, 0, 0);
-        return false;
+    	return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.drivetrain.move(0, 0, 0);
+
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.drivetrain.move(0, 0, 0);
+    
     }
 }
